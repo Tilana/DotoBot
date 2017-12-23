84eb1f5f523de98d3f6bffa7dda013a8e83ec346
tilana <NatalieWidmann@gmx.de>
2017-12-23 18:05:50 +0100

    two layer CNN with ReLU activation and max pooling

--- Q_Net.py
+++ Q_Net.py
@@ -4,14 +4,25 @@ class Q_Net:
 
     def __init__(self, input_size=None, output_size=None):
 
-        self.inputs = tf.placeholder(shape=[1, input_size], dtype=tf.float32)
-        self.W = tf.Variable(tf.random_uniform([input_size, output_size], 0, 0.01))
-        self.Q = tf.matmul(self.inputs, self.W)
-        self.predict = tf.argmax(self.Q, 1)
 
-        self.next_Q = tf.placeholder(shape=[1,output_size], dtype=tf.float32)
-        self.loss = tf.reduce_sum(tf.square(self.next_Q - self.Q))
-        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+        self.inputs = tf.placeholder(shape=[1, input_size, 1], dtype=tf.float32)
+
+        self.conv = tf.layers.conv1d(inputs=self.inputs, filters=4, kernel_size=(5), padding='same', activation=tf.nn.relu)
+        self.pool1 = tf.layers.max_pooling1d(inputs=self.conv, pool_size=(2), strides=1)
+
+        self.conv2 = tf.layers.conv1d(inputs=self.pool1, filters=4, kernel_size=[5], padding='same', activation=tf.nn.relu)
+        self.pool2 = tf.layers.max_pooling1d(inputs=self.conv2, pool_size=[2], strides=1)
+
+        self.pool2_flat = tf.reshape(self.pool2, [-1, 20*4])
+        self.dense = tf.layers.dense(inputs=self.pool2_flat, units=30, activation=tf.nn.relu)
+
+        self.logits = tf.layers.dense(inputs=self.dense, units=20)
+
+        self.predict = tf.argmax(self.logits, axis=1)
+
+        self.target_Q = tf.placeholder(shape=[1,output_size], dtype=tf.float32)
+        self.loss = tf.reduce_sum(tf.square(self.target_Q - self.logits))
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.005)
         self.update = self.optimizer.minimize(self.loss)
 
 
